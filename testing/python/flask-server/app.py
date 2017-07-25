e42b189f9154154237c3913c2a4604b480d34146
kritiagg <kriti.dce@gmail.com>
2017-07-25 15:24:58 -0700

    running the server

--- testing/python/flask-server/app.py
+++ testing/python/flask-server/app.py
@@ -1,17 +1,52 @@
-from flask import Flask, url_for
+import os
+# We'll render HTML templates and access data sent by POST
+# using the request object from flask. Redirect and url_for
+# will be used to redirect the user once the upload is done
+# and send_from_directory will help us to send/show on the
+# browser the file that the user just uploaded
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+
+# Initialize the Flask application
 app = Flask(__name__)
 
-@app.route('/')
-def api_root():
-    return 'Welcome'
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 
-@app.route('/articles')
-def api_articles():
-    return 'List of ' + url_for('api_articles')
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    print("The uploaded file is:",filename)
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/articles/<articleid>')
-def api_article(articleid):
-    return 'You are reading ' + articleid
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/api/hello')
+def index():
+    return "Hello!"
+
+
+# Route that will process the file upload
+@app.route('/api', methods=['POST'])
+def upload():
+    print("file upload request")
+    # Get the name of the uploaded file
+    file = request.files['file']
+    print("file:",file)
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return filename
+    return "Bad file"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',debug = True)
